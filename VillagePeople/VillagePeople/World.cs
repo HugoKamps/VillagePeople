4b46bf175b7395f156229840f3ed47262eddb384
Hugo Kamps <hugokamps6@hotmail.com>
2017-02-28 12:54:36 +0100

    Basisklassen toegevoegd

---
+++ VillagePeople/VillagePeople/World.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VillagePeople {
+    class World {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public World(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            Init();
+        }
+
+        public void Init()
+        {
+            
+        }
+
+        public void Update()
+        {
+            
+        }
+
+        public void Render()
+        {
+            
+        }
+    }
+}
